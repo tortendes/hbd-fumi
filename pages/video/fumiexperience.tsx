525172629cc58d99aceadfc8a872adbf9e3cd739
Jarelle Emmanuel Payad Andulan <torten@proton.me>
2022-09-20 11:46:07 +0800

    LATE PUSH MAKE IT AAAAA

--- pages/video/fumiexperience.tsx
+++ pages/video/fumiexperience.tsx
@@ -11,7 +11,11 @@ const VideoExperience: NextPage = (): JSX.Element => {
                 <meta name="description" content="This is the website responsible for all the content generated for Fumi's birthday" />
             </Head>
             <div className={styles.main}>
-                <h1>Available at: sometime today</h1>
+                <h1>The Fumi Kiritsugu Experience</h1>
+                <div className={styles.videoHolder}>
+                    <iframe className={styles.video} src="https://www.youtube.com/embed/-87eiuj_DZU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <iframe className={styles.chat} height="1000px" src="https://www.youtube.com/live_chat?v=-87eiuj_DZU&embed_domain=localhost" frameBorder="0"></iframe>
+                </div>
                 <Link href="/video" passHref>
                     <button className={styles.button}>
                         Go Back
