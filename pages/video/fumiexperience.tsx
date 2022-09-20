091b439ce34f8fdc576bb55761ea353df7c4d4d6
Jarelle Emmanuel Payad Andulan <torten@proton.me>
2022-09-20 11:47:52 +0800

    IFRAME FIX AAA

--- pages/video/fumiexperience.tsx
+++ pages/video/fumiexperience.tsx
@@ -14,7 +14,7 @@ const VideoExperience: NextPage = (): JSX.Element => {
                 <h1>The Fumi Kiritsugu Experience</h1>
                 <div className={styles.videoHolder}>
                     <iframe className={styles.video} src="https://www.youtube.com/embed/-87eiuj_DZU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <iframe className={styles.chat} height="1000px" src="https://www.youtube.com/live_chat?v=-87eiuj_DZU&embed_domain=localhost" frameBorder="0"></iframe>
+                    <iframe className={styles.chat} height="1000px" src="https://www.youtube.com/live_chat?v=-87eiuj_DZU&embed_domain=hbd-fumi.torten.xyz" frameBorder="0"></iframe>
                 </div>
                 <Link href="/video" passHref>
                     <button className={styles.button}>
